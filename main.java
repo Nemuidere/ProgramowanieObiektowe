68e0b665fe4db3b50943859a807a9df0a681c948
Nemuidere <olokoxthelegend@gmail.com>
2025-05-13 19:08:55 +0200

    Commit uwu

---
+++ main.java
@@ -0,0 +1,9 @@
+package ProgramowanieObiektowe;
+
+public class main {
+    int a = 3;
+    int b = 5;
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
